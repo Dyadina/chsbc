5c5ee586bd051be38fb322b232aab428bc8f782f
Nekh_Ruslan <rpykalo2000@gmail.com>
2023-06-01 13:54:37 +0300

    Fibonachi

---
+++ Практичне завдання 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практичне_завдання_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 1;
+            int n2 = 1;
+            Console.Write(n1 + " " + n2 + " ");
+            for (int i = 2; i < 38; i++) 
+            {
+                int n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                Console.Write(n3 + " ");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Практичне завдання 1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Практичне завдання 1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Практичне завдання 1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("c58b2eb5-c743-4e94-a4e7-6ff00b89961c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
